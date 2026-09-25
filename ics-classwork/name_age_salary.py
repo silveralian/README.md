8a352d69de146f1d11403a7e7af122d13b1edbad
silveralian <tambethany20@gmail.com>
2026-09-25 16:57:39 -0400

    Add name, age, and salary input functionality

---
+++ ics-classwork/name_age_salary.py
@@ -0,0 +1,9 @@
+name = input("Hello. What is your name? ")
+
+age = int(input(f"Hi, {name}! How old are you? "))
+
+print(f"So you're {age}, eh? That's not old at all!")
+
+salary = float(input(f"How much do you make, {name}? "))
+
+print(f"{salary}! I hope that's per hour and not per year! LOL!")
