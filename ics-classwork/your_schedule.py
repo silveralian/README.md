8883bde4717eb510644d35dbc9784dd741fb729a
silveralian <tambethany20@gmail.com>
2026-09-24 20:47:00 -0400

    Add files via upload

---
+++ ics-classwork/your_schedule.py
@@ -0,0 +1,34 @@
+course1 = "Technology and the Skilled Trades"
+teacher1 = "Ms.Fiore"
+
+course2 = "Computer Science"
+teacher2 = "Mr Gallo"
+
+course3 = "Science"
+teacher3 = "Mr Di Carlo"
+
+course4 = "History"
+teacher4 = "Mrs.Nesci"  
+
+course5 = "English"
+teacher5 = "ateacher"
+
+course6 = "math"
+teacher6 = "bteacher"
+
+course7 = "Career and Civics"
+teacher7 = "cteacher"
+
+course8 = "Religion"
+teacher8 = "dteacher"
+
+print("+--------------------------------------------------+")
+print(f"| 1 | {course1:>26} | {teacher1:>15} |")
+print(f"| 2 | {course2:>26} | {teacher2:>15} |")
+print(f"| 3 | {course3:>26} | {teacher3:>15} |")
+print(f"| 4 | {course4:>26} | {teacher4:>15} |")
+print(f"| 5 | {course5:>26} | {teacher5:>15} |")
+print(f"| 6 | {course6:>26} | {teacher6:>15} |")
+print(f"| 7 | {course7:>26} | {teacher7:>15} |")
+print(f"| 8 | {course8:>26} | {teacher8:>15} |")
+print("+--------------------------------------------------+")
